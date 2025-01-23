51bb27d27496c2bcdc7071f6f3b0fa1986a3dd23
Pocky <Pissawat2551@gmail.com>
2025-01-23 09:35:39 +0700

    finished third python assignment

--- third.py
+++ third.py
@@ -1,10 +1,12 @@
 print("temperature converter")
 degree = float(input("insert degree: "))
-degreeType = str(input("insert temperture type: "))
+degreeType = str(input("insert temperature type: "))
 
-if degreeType ==  "f" or degreeType == "F" :
-  convertDegree = (5 * (degree - 32)) / 9
-  print(convertDegree, "degree Fahrenheit")
-elif degreeType == "c" or degreeType == "C" :
+if degreeType ==  "c" or degreeType == "C" :
   convertDegree = (9 * degree + (32 * 5)) / 5
+  print(convertDegree, "degree Fahrenheit")
+elif degreeType == "f" or degreeType == "F" :
+  convertDegree = (5 * (degree - 32)) // 9
   print(convertDegree, "degree Celsius")
+else :
+  print("error")
