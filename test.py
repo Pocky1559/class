e24fa2e7e47d4d470a2831ecb4b5c7a02a77b7a8
Pocky <pissawt2551@gmaill.com>
2025-01-31 08:23:22 +0700

    complete the test file

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
+# While loop
+
 a = int(input())
 i= 1
 while i < a :
   print((str(i)) * i)
   i += 1
+
+number = int(input('Enter a number: '))
+while number != 0 :
+  print('your number is', number)
+  number = int(input('Enter a number: '))
+print('The end.')
+
+x = int(input())
+while True :
+  if x == 10 :
+    print('***10***')
+    break
+  elif x > 10 :
+    print(x)
+    x -= 1
+  elif x < 10 :
+    print(x)
+    x += 1
