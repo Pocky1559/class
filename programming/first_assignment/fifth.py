336d0cd7ceb37b8e2b30214639dc949bcea47d84
Pocky <Pissawat2551@gmail.com>
2025-02-06 09:53:40 +0700

    change the code in fifth python file a little bit

--- programming/first_assignment/fifth.py
+++ programming/first_assignment/fifth.py
@@ -5,9 +5,9 @@ while password != 10 :
 
   if password > 10 :
     print('your number is higher than the password')
-    password = int(input('please try again: '))
   else :
     print('your number is lesser than the password')
-    password = int(input('please try again: '))
+    
+  password = int(input('please try again: '))
 
 print('correct')
