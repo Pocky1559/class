e430e9926b4c32693fc4c2a0c2870128860b1e9d
Pocky <pocky1559@proton.me>
2025-02-17 18:54:24 +0700

    add chapter 3 in novel.py

--- programming/second_assignment/novel.py
+++ programming/second_assignment/novel.py
@@ -43,7 +43,7 @@ def set_name() :
   global name
   while True :
     reset_terminal()
-    # time.sleep(1)
+    time.sleep(1)
 
     print(f"Do you want to use {name} name or create your own?", "\n")
     name_confirm = input("Type \"Y\" for change the name, or Type \"N\" for use the default name: ")
@@ -59,7 +59,7 @@ def set_name() :
         break
       else :
         print('\nName cannot be empty please put your name again.')
-        # time.sleep(2.5)
+        time.sleep(2.5)
         continue
 
 
@@ -70,7 +70,7 @@ def set_name() :
 
     else :
       print("Press type Y or N.")
-      # time.sleep(1.5)
+      time.sleep(1.5)
 
 def delay(text) :
   return len(text) / 20
@@ -78,7 +78,7 @@ def delay(text) :
 def show_dialog(array) :
   for text in array :
     print(text)
-    # time.sleep(delay(text))
+    time.sleep(delay(text))
 
 def show_option(array) :
   for text in array :
@@ -89,11 +89,11 @@ reset_terminal()
 welcome_screen()
 
 reset_terminal()
-# time.sleep(1)
+time.sleep(1)
 print(game_name)
-# time.sleep(3.5)
+time.sleep(3.5)
 reset_terminal()
-# time.sleep(1.5)
+time.sleep(1.5)
 
 # Chapter 1
 show_dialog([
@@ -156,7 +156,7 @@ while True :
 
   else :
     print("Please put the number 1-3")
-    # time.sleep(2)
+    time.sleep(2)
 
 input('\nPlease enter to go to the next chapter')
 reset_terminal()
@@ -223,4 +223,69 @@ while True :
 
   else :
     print("Please put the number 1-3")
-    # time.sleep(2)
+    time.sleep(2)
+
+input('\nPlease enter to go to the next chapter')
+reset_terminal()
+
+show_dialog([
+  'Chapter 3: The Secret Revealed',
+  '',
+  'Over the next few days, you and Luna grow closer.',
+  'She starts showing up at the park every evening, and you find yourself looking forward to seeing her.',
+  'But one night, something strange happens.',
+  '',
+  'As you play a new song for her, the air around you seems to shimmer.',
+  'Luna’s eyes glow faintly, and for a moment, you see a pair of translucent wings behind her.',
+  '',
+  f'{name}: "Luna... what are you?"',
+  f'Luna: (hesitates) "I... I’m not human, {name}. I’m a star spirit.',
+  'I came to Earth to experience life, but my time here is limited."',
+  '',
+  'You’re stunned.',
+  'Luna looks at you with a mix of fear and hope, waiting for your reaction.',
+  '',
+  'What do you say?',
+  ''
+])
+
+show_option([
+  '1. "It doesn’t matter what you are. I care about you, Luna."',
+  '2. "How long do you have left here?"',
+  '3. "This is too much. I need some time to think."'
+])
+
+while True :
+  chapter = input('Choose your response: ')
+
+  print('')
+  if chapter == '1' :
+    ending_score["ending1"] += 1
+    show_dialog([
+      'Luna’s eyes fill with tears, and she smiles.',
+      'Luna: "You really mean that, don’t you?"',
+      'She takes your hand, and you feel a warm, glowing energy pass between you.'
+    ])
+    break
+  
+  elif chapter == '2' :
+    ending_score["ending2"] += 1
+    show_dialog([
+      'Luna looks down, her voice barely a whisper.',
+      'Luna: "One month. That’s all I have left."',
+      'The weight of her words hangs in the air, and you feel a pang of sadness.'
+    ])
+    break
+  
+  elif chapter == '3' :
+    ending_score['ending3'] += 1
+    show_dialog([
+      'Luna nods, her expression resigned.',
+      'Luna: "I understand. Take all the time you need."',
+      'She stands and walks away, leaving you alone in the park.'
+    ])
+    break
+
+  else :
+    print("Please put the number 1-3")
+    time.sleep(2)
